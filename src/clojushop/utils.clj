2eda3d699c6ca4fa7d73c63797c7ec86337b094f
ischuetz <ivanschuetz@gmail.com>
2014-05-31 23:44:51 +0200

    Add explanations

--- src/clojushop/utils.clj
+++ src/clojushop/utils.clj
@@ -3,39 +3,45 @@
 
 (defn map-var
   "Utility function, serves 2 purposes:
-1. If val is a sequence, applies function to elements using (map), otherwise directly to val
-2. If val is a sequence ensures that the type of the returned sequence is the same as of val
+- If val is a sequence, applies function to elements using (map), otherwise directly to val
+- If val is a sequence ensures that the type of the returned sequence is the same as of val
 supported sequences: list, map, vector"
-;TODO are there better solutions for this?
+  ; TODO better implementation?
   [function val]
   (if (or (seq? val) (vector? val))
     (let [res (map function val)]
+      (when (seq? val) (into '() res))
       (when (vector? val) (into [] res)))
     (let [res (function val)]
       (when (map? val) (into {} res)))))
 
-;note had problems naming parameter "map" that's why "mmap"
-(defn replace-key [old-key new-key mmap]
-  (map (fn [[k  v]] [
-                     (if (= k old-key) new-key k) ;key
-                     v ;value
-                     ]
+(defn replace-key
+  "Replaces a key in a map"
+  [old-key new-key mmap]
+  (map (fn [[k  v]] [(if (= k old-key) new-key k) v]
          ) mmap))
 
-(defn map-keys [keys-map mmap]
+(defn map-keys
+  "Maps the keys of a map mmap to new keys, using the map keys-map"
+  [keys-map mmap]
   (into {} (map (fn [[k v]] [(k keys-map) v]) mmap)))
 
 
 
-(defmulti filter-map-keys (fn [keys mmap] (class keys)))
+(defmulti filter-map-keys
+  "Filters and maps keys of map mmap, using keys. The behaviour of mapping depends of the type of keys."
+  (fn [keys mmap] (class keys)))
 
-;;   "Filters a mmap with keys from keys-map, and then maps keys from mmap to values of keys-map
-;; Example: 
-;; (filter-and-map-keys {:a :newa :b :newb} {:a 1 :b 2 :c 3}) ==> {:newa 1 :newb 2}"
-(defmethod filter-map-keys clojure.lang.PersistentHashMap
+(defmethod filter-map-keys
+  clojure.lang.PersistentHashMap
   [keys-map mmap]
+  "Filters and maps keys of map mmap, using the map keys-map.
+  Example: 
+  (filter-and-map-keys {:a :newa :b :newb} {:a 1 :b 2 :c 3}) ==> {:newa 1 :newb 2}"
   (map-keys keys-map (select-keys mmap (keys keys-map))))
 
-(defmethod filter-map-keys clojure.lang.PersistentVector
+(defmethod filter-map-keys
+  clojure.lang.PersistentVector
   [keys-vector mmap]
+  "Filters the keys of map mmap using the vector keys-vector. No mapping is done / the keys are mapped to themselves"  
   (select-keys mmap keys-vector))
